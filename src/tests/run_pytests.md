1074f2a819c0dd3bcfc470057e3c0d250ae0806d
william cross <william.cross@stfc.ac.uk>
2026-06-23 13:14:19 +0100

    created the pytest structure and created the search_redirect tests

---
+++ src/tests/run_pytests.md
@@ -0,0 +1,4 @@
+cd src
+python -m pytest
+
+(-s to view prints)
--- src/tool_functionality/search_redirect.py
+++ src/tool_functionality/search_redirect.py
@@ -1,7 +1,9 @@
 from .common import check_link
 from urllib.parse import quote_plus
 
-def search_redirect(query: str) -> str:    
+def search_redirect(query: str) -> str:
+    if query is None:
+        return("Query must not be a None type")
     redirect_link = "https://cse.google.com/cse?cx=009617667314646343139:y6z_ljohx30#gsc.tab=0&gsc.q=" + quote_plus(query)
     confirmation = check_link(redirect_link)
     
